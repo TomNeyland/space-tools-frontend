56b52f894477e697a3c8c89e96c10e2bbabf0d21
Skessler <stvnksslr@gmail.com>
2015-07-21 23:06:10 -0400

    chore(build tools final): finished setting up unit testings, es6 + browserify and sass tasks.

--- gulp/config.js
+++ gulp/config.js
@@ -1,13 +1,59 @@
-/**
- * Created by skessler on 7/21/15.
- */
-
 'use strict';
 
-var fs = require('fs'),
-    onlyScripts = require('./util/scriptFilter'),
-    tasks = fs.readdirSync('./gulp/tasks/').filter(onlyScripts);
+module.exports = {
 
-tasks.forEach(function (task) {
-    require('./tasks/' + task);
-});
+    'browserPort': 3000,
+    'UIPort': 3001,
+    'serverPort': 3002,
+
+    'styles': {
+        'src': 'public/**/*.scss',
+        'dest': 'build'
+    },
+
+    'scripts': {
+        'src': 'public/**/*.js',
+        'dest': 'build'
+    },
+
+    'images': {
+        'src': 'public/assets/images/**/*',
+        'dest': 'build/assets/images'
+    },
+
+    'fonts': {
+        'src': ['public/fonts/**/*'],
+        'dest': 'build/fonts'
+    },
+
+    'views': {
+        'watch': [
+            'public/index.html',
+            'public/**/*.html'
+        ],
+        'src': 'public/**/*.html',
+        'dest': 'public'
+    },
+
+    'gzip': {
+        'src': 'build/**/*.{html,xml,json,css,js,js.map}',
+        'dest': 'build/',
+        'options': {}
+    },
+
+    'dist': {
+        'root': 'build'
+    },
+
+    'browserify': {
+        'entries': ['./public/main.js'],
+        'bundleName': 'main.js',
+        'sourcemap': true
+    },
+
+    'test': {
+        'karma': 'test/karma.conf.js',
+        'protractor': 'test/protractor.conf.js'
+    }
+
+};
--- gulp/index.js
+++ gulp/index.js
@@ -3,3 +3,11 @@
  */
 
 'use strict';
+
+var fs = require('fs'),
+    onlyScripts = require('./util/scriptFilter'),
+    tasks = fs.readdirSync('./gulp/tasks/').filter(onlyScripts);
+
+tasks.forEach(function (task) {
+    require('./tasks/' + task);
+});
